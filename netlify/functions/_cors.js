1c4a907dbb4433a275e13b3e7b82a2f58d5e62d4
jojogarcia24 <Joseph.Garcia@DFWEliteLiving.com>
2025-10-27 21:55:31 -0500

    Update CORS headers and add max age

--- netlify/functions/_cors.js
+++ netlify/functions/_cors.js
@@ -1,18 +1,18 @@
 // netlify/functions/_cors.js
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST,OPTIONS",
-  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Allow-Origin": "*",           // set to your Softr origin in prod
+  "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+  "Access-Control-Allow-Headers": "authorization, content-type",
+  "Access-Control-Max-Age": "86400",
+  Vary: "Origin"
 };
 
 export function withCors(handler) {
   return async (event, context) => {
     if (event.httpMethod === "OPTIONS") {
-      // Preflight response
-      return { statusCode: 204, headers: corsHeaders };
+      return { statusCode: 204, headers: corsHeaders, body: "" };
     }
     const res = await handler(event, context);
-    // Ensure CORS headers on normal responses
     return { ...res, headers: { ...(res.headers || {}), ...corsHeaders } };
   };
 }
